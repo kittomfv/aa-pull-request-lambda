2751e835aef29b54912d2308b875b8ab9e29e7fe
bichkhe <mr.bichkhe@gmail.com>
2024-03-29 14:22:49 +0700

    update username

--- README.md
+++ README.md
@@ -13,13 +13,16 @@ cargo lambda build
 
 # Deploy
 
+```
 cargo lambda deploy --iam-role arn:aws:iam::747194874562:role/rust-role --enable-function-url
+
+```
 
 
 # Setup environmnent variable 
 
 
 
-Setup 2 variable `GITHUB_ACCESS_TOKEN` and `SLACK_NOTIFICATION_CHANNEL`
+Setup 2 variable `GITHUB_ACCESS_TOKEN` and `SLACK_NOTIFICATION_CHANNEL_FE` `SLACK_NOTIFICATION_CHANNEL_BE`
 
 Lambda URL Trigger: https://2tajlxmufgyyohlxg5vb67cjoy0zekcn.lambda-url.ap-northeast-1.on.aws/
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,11 @@ use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
 
 use lazy_static::lazy_static;
 use serde::Deserialize;
-use slack_messaging::{blocks::Section, Message};
-use std::collections::HashMap;
+use slack_messaging::{
+    blocks::{elements::Text, Block, Section},
+    Message,
+};
+use std::{collections::HashMap, rc::Rc, sync::Arc};
 use string_builder::Builder;
 use tokio;
 
@@ -31,8 +34,13 @@ lazy_static! {
         m.insert("dustinmfv", "U02J4N7C0TE");
         m.insert("pirlomnfw", "U03H8R4HF1C");
         m.insert("frankymfv", "U03ECBB3FGR");
-        m.insert("nicholasmfv", "U03QJBJ951V");
+        m.insert("ThetaMFV", "U02QSMJ1BRQ");
         m.insert("leonmfv", "U03ALAZSSRE");
+        m.insert("nicholas", "U03QJBJ951V");
+        m.insert("augustmfv", "U0486QG6A9W");
+        m.insert("Antom-Nguyen", "U036V63ML5Q");
+        m.insert("shajMF", "U04N576LURH");
+        m.insert("mfv-brian", "U02B92JJMA7");
         m
     };
 }
@@ -66,26 +74,32 @@ impl PRs {
         }
         Self { prs: filter_prs }
     }
-    fn convert_slack_data(&self) -> Result<String, anyhow::Error> {
-        let mut builder = Builder::default();
+    fn convert_slack_data(&self) -> Result<Vec<String>, anyhow::Error> {
         let now: DateTime<Utc> = Utc::now();
-        for pr in &self.prs {
-            let local_updated_at: DateTime<Local> = DateTime::from(pr.updated_at);
-            let delta = now.signed_duration_since(pr.created_at);
-            let tk = format!(
-                "<@{}>: *{}* days -  _{}_ last updated: *{}* - <{}| see more>\n ",
-                MEMBERS
-                    .get(pr.user.login.as_str())
-                    .unwrap_or(&pr.user.login.as_str()),
-                delta.num_days(),
-                pr.title,
-                local_updated_at.format("%d/%m/%Y - %H:%M VNT"),
-                pr.html_url
-            );
+        let mut results: Vec<String> = vec![];
+        let chunk_prs = self.prs.chunks(4);
+        for chunk in chunk_prs {
+            let mut builder = Builder::default();
+            for pr in chunk {
+                let local_updated_at: DateTime<Local> = DateTime::from(pr.updated_at);
+                let delta = now.signed_duration_since(pr.created_at);
+                let tk = format!(
+                    "<@{}>: *{} days*  -  _{}_ last updated: *{}* - <{}| see more>\n ",
+                    MEMBERS
+                        .get(pr.user.login.as_str())
+                        .unwrap_or(&pr.user.login.as_str()),
+                    delta.num_days(),
+                    pr.title,
+                    local_updated_at.format("%d/%m/%Y - %H:%M VNT"),
+                    pr.html_url
+                );
 
-            builder.append(tk);
+                builder.append(tk);
+            }
+            results.push(builder.string().unwrap())
         }
-        Ok(builder.string().unwrap())
+
+        Ok(results)
     }
 }
 
@@ -101,15 +115,59 @@ struct PullRequestParams {
 /// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
 async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
     println!("start main:{:?}", event);
-    let slack_channel = match std::env::var("SLACK_NOTIFICATION_CHANNEL") {
+    let slack_channel = match std::env::var("SLACK_NOTIFICATION_CHANNEL_BE") {
+        Ok(v) => v,
+        Err(e) => panic!("Err: no config SLACK_NOTIFICATION_CHANNEL variable: {}", e),
+    };
+    let slack_channel_fe = match std::env::var("SLACK_NOTIFICATION_CHANNEL_FE") {
         Ok(v) => v,
         Err(e) => panic!("Err: no config SLACK_NOTIFICATION_CHANNEL variable: {}", e),
     };
     println!("slack channel:{}", slack_channel);
-    // event.query_string_parameters()
-    // let (_, body) = event.into_parts();
-    // let pr_params: PullRequestParams = serde_json::from_slice(&body)?;
-    // println!("pr_params:{:#?}", pr_params);
+    let slack_fe = Arc::new(slack_channel_fe);
+
+    let ops = vec![
+        (
+            "AA Backend",
+            "https://api.github.com/repos/moneyforwardvietnam/asset_accounting_backend/pulls",
+            slack_channel,
+        ),
+        (
+            "AA Frontend",
+            "https://api.github.com/repos/moneyforwardvietnam/asset_accounting_frontend/pulls",
+            slack_fe.clone().to_string(),
+        ),
+        (
+            "AA FE Common",
+            "https://api.github.com/repos/moneyforwardvietnam/aa_common/pulls",
+            slack_fe.clone().to_string(),
+        ),
+    ];
+    let mut tasks = Vec::with_capacity(ops.len());
+    for op in ops {
+        // This call will make them start running in the background
+        // immediately.
+        tasks.push(tokio::spawn(run_each(
+            op.0.to_string(),
+            op.1.to_string(),
+            op.2.to_string(),
+        )));
+    }
+    let mut outputs = Vec::with_capacity(tasks.len());
+    for task in tasks {
+        outputs.push(task.await.unwrap()?);
+    }
+    println!("{:?}", outputs);
+
+    Ok(Response::new(Body::Text("success".to_string())))
+}
+
+async fn run_each(
+    repo_name: String,
+    github_url: String,
+    slack_channel: String,
+) -> Result<Response<Body>, Error> {
+    println!("github_url:{github_url}, slack:{slack_channel}");
     let github_access_token = match std::env::var("GITHUB_ACCESS_TOKEN") {
         Ok(v) => v,
         Err(e) => panic!("Err: no config GITHUB_ACCESS_TOKEN variable: {}", e),
@@ -117,7 +175,7 @@ async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
 
     let client = reqwest::Client::new();
     let list_prs = client
-        .get("https://api.github.com/repos/moneyforwardvietnam/asset_accounting_backend/pulls")
+        .get(github_url)
         .bearer_auth(github_access_token)
         .header("X-GitHub-Api-Version", "2022-11-28")
         .header("Accept", "application/vnd.github+json")
@@ -133,18 +191,28 @@ async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
 
     let tickets = PRs::filter(1, list_prs);
     let now: DateTime<Utc> = Utc::now();
-    let message = Message::new()
-        .push_block(Section::new().set_text_mrkdwn(format!(
-            "*[{}] :alert: :sos: Report Pull Requests are > {} days:*",
-            now.format("%d/%m/%Y"),
-            2
-        )))
-        .push_block(Section::new().set_text_mrkdwn(tickets.convert_slack_data().unwrap()));
+    let mut message = Message::new().push_block(Section::new().set_text_mrkdwn(format!(
+        "*[{}] {} :alert: :sos: Report Pull Requests are > {} days:*",
+        now.format("%d/%m/%Y"),
+        repo_name,
+        2
+    )));
 
-    let client2 = reqwest::ClientBuilder::new().build()?;
+    let blocks = tickets
+        .convert_slack_data()
+        .unwrap()
+        .iter()
+        .map(|block| Block::Section(Box::new(Section::new().set_text_mrkdwn(block))))
+        .collect::<Vec<Block>>();
+
+    let _ = blocks
+        .iter()
+        .for_each(|b| message = message.clone().push_block(b.clone()));
+
     let data = serde_json::to_string(&message)?;
     println!("{}", data);
-    let _ = client2
+    let _ = reqwest::ClientBuilder::new()
+        .build()?
         .post(slack_channel)
         .header("Content-type", "application/json")
         .body(data.clone())
